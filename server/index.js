bcd27be19026498ddf1e4a56856ac990648d9a12
Angela Chang <ajkchang@gmail.com>
2018-04-13 02:23:00 -0700

    updated server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,16 @@
-import express from 'express';
+const express = require('express');
+const request = require('request');
+var querystring = require('querystring');
 
+const app = express();
+
+let redirectUri = process.env.REDIRECT_URI || 'http://localhost:3000/callback';
+
+
+
+
+const portNum = process.env.PORT || 3000;
+app.listen(portNum, function() {
+  console.log('listening on port 3000!');
+});
 
