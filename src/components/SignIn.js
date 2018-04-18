10c4ea0bdd5d59e3b377cb91bbd75cd442bceccb
Angela Chang <ajkchang@gmail.com>
2018-04-17 17:11:58 -0700

    updated sign in div

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -3,13 +3,11 @@ import React, { Component } from 'react';
 class SignIn extends Component {
   render() {
     return (
-      <div>
-        <p className="App-intro">
-          <p>Please sign in</p>
-          <p>username <input type="text" /> </p>
-          <p>password <input type="text" /> </p>
-          <p>Don't have an account? <button>Create one </button></p>
-        </p>
+      <div className="App-intro">
+        <p>Please sign in</p>
+        <p>username <input type="text" /> </p>
+        <p>password <input type="text" /> </p>
+        <p>Don't have an account? <button>Create one </button></p>
       </div>
     );
   }
