23a5eed2a107a21bdbcb46bfa4791e0c9daa7a7a
Angela Chang <ajkchang@gmail.com>
2018-04-18 00:56:16 -0700

    updating sign in

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import badge from '../wordmark+badgeCMYK.jpg';
 
 class SignIn extends Component {
   render() {
     return (
       <div className="App-intro">
-        <p>Please sign in</p>
+        <p>Sign in</p>
         <p>username <input type="text" /> </p>
         <p>password <input type="text" /> </p>
         <p>Don't have an account? <button>Create one </button></p>
+        or
+        <p>Sign in with <img src={badge} className="signin-logo" alt="badge"/></p>
       </div>
     );
   }
