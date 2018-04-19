c1dfde09e924712761b6508c3449bbcafda52310
Angela Chang <ajkchang@gmail.com>
2018-04-18 17:46:55 -0700

    made sign in with only pinterest

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -5,11 +5,6 @@ class SignIn extends Component {
   render() {
     return (
       <div className="App-intro">
-        <p>Sign in</p>
-        <p>username <input type="text" /> </p>
-        <p>password <input type="text" /> </p>
-        <p>Don't have an account? <button>Create one </button></p>
-        or
         <p>Sign in with <img src={badge} className="signin-logo" alt="badge"
           onClick={() => window.location = 'https://localhost:8888/login'}/></p>
       </div>
