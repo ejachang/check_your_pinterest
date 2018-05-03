8e3d8ffcfa00bd9f1771b80051f820a40fa5b7b9
Angela Chang <ajkchang@gmail.com>
2018-05-03 13:15:40 -0700

    still working on Routing

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Link, Route, Redirect } from 'react-router-dom';
+import Pins from './Pins';
 
 const Board = (props) => {
+  // debugger;
+  //fetch info from server
+  //redirect to Pins comp link
   return (
     <div id="boardwrapper">
       <Link to={'/boards/' + props.singleboard.name} style={{ textDecoration: 'none' }}>
-        <h5 id="singleboardname"><a>{props.singleboard.name}</a></h5>
+        <h5 id="singleboardname">{props.singleboard.name}</h5>
+        {/* <Route exact path={}/> */}
+        <img 
+          id="singleboardimage" 
+          src={props.singleboard.image['60x60']['url']}
+        />
       </Link>
-      <img 
-        id="singleboardimage" 
-        src={props.singleboard.image['60x60']['url']}
-        onClick={() => { window.location = props.singleboard.url; }}
-      />
+      <Route path={'/boards/' + props.singleboard.name} component={Pins}/>
     </div>
   );
 };
--- src/components/Pins.js
+++ src/components/Pins.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Pins = (props) => {
   return (
     <div>
-
+      
     </div>
   );
 
