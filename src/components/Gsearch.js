b5a6faf876bbc04eb8a84fb64df614948ea36a85
Angela Chang <ajkchang@gmail.com>
2018-05-08 12:06:54 -0700

    adding Google Search comp

---
+++ src/components/Gsearch.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+/*
+newWindow	Boolean	Specifies whether the results page opens in a new window. Default: false.
+
+*/
+
+const Gsearch = () => {
+
+};
+
+export default Gsearch;
