407c274ae5c1da0b60fabdd78dee87e4373073cf
Angela Chang <ajkchang@gmail.com>
2018-08-14 19:44:39 -0700

    fixed webpack4

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,43 @@
-// var path = require('path');
-// var webpack = require('webpack');
-// var SRC_DIR = path.join(__dirname, 'client/src');
-// var DIST_DIR = path.join(__dirname, 'client/dist');
+var path = require('path');
+var webpack = require('webpack');
+var SRC_DIR = path.join(__dirname, 'src');
+var DIST_DIR = path.join(__dirname, 'dist');
 
-// module.exports = {
-//   entry: `${SRC_DIR}/index.js`,
-//   output: {
-//     path: DIST_DIR,
-//     filename: 'bundle.js'
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.jsx?/,
-//         include: SRC_DIR,
-//         loader: 'babel-loader',
-//       }
-//     ]
-//   },
-//   resolve: {
-//     extensions: ['.jsx', '.js', '.css'],
-//   },
+module.exports = {
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/, 
+        include: SRC_DIR,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 5000
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.css'],
+  },
   
-// };
+};
